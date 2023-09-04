34f2ab5ecc04b0d98d5a5bae241714c7bea3bde6
elvisuriel <urielmarciales@gmil.com>
2023-09-03 19:23:27 -0500

    cambios

--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { FC } from "react";
 import { Grid } from "@mui/material";
 import { IProduct } from "../../interfaces";
@@ -17,23 +16,3 @@ export const ProductList: FC<Props> = ({ products }) => {
     </Grid>
   );
 };
-=======
-import { FC } from "react";
-import { Grid } from "@mui/material";
-import { IProduct } from "../../interfaces";
-import { ProductCard } from ".";
-
-interface Props {
-  products: IProduct[];
-}
-
-export const ProductList: FC<Props> = ({ products }) => {
-  return (
-    <Grid container spacing={8}>
-      {products.map((product) => (
-        <ProductCard key={product.slug} product={product} />
-      ))}
-    </Grid>
-  );
-};
->>>>>>> 1f6200a577589790a4dfb53472758544a7b282d1
