46362519390da130c471996439fc0b481cea6612
elvisuriel <urielmarciales@gmil.com>
2023-09-04 22:39:42 -0500

    agregar logo y estilos

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,4 +1,4 @@
-import react from "react";
+import React, { useState } from "react";
 import NextLink from "next/link";
 import {
   Grid,
@@ -16,25 +16,28 @@ interface Props {
   product: IProduct;
 }
 
-export const ProductCard: react.FC<Props> = ({ product }) => {
-  const [isHovered, setIsHovered] = react.useState(false);
+export const ProductCard: React.FC<Props> = ({ product }) => {
+  const [isHovered, setIsHovered] = useState(false);
 
-  const productImage = react.useMemo(() => {
-    return isHovered
-      ? `products/${product.images[1]}`
-      : `products/${product.images[0]}`;
-  }, [isHovered, product.images]);
+  const productImage = isHovered
+    ? `products/${product.images[1]}`
+    : `products/${product.images[0]}`;
 
   return (
     <Grid
       item
-      xs={6}
-      sm={4}
+      xs={12}
+      sm={6}
+      md={4}
+      lg={4}
+      sx={{ px: 2, pb: 2 }} // Añade margen en cada lado de la tarjeta
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <Card sx={{ maxWidth: "250px" }}>
-        <NextLink href="/product/slug" passHref prefetch={false}>
+      <Card sx={{ maxWidth: 200, mx: "auto" }}>
+        {" "}
+        {/* Ajusta el ancho máximo y centra la tarjeta */}
+        <NextLink href={`/product/${product.slug}`} passHref prefetch={false}>
           <Link>
             <CardActionArea>
               <CardMedia
@@ -46,8 +49,7 @@ export const ProductCard: react.FC<Props> = ({ product }) => {
             </CardActionArea>
           </Link>
         </NextLink>
-
-        <Box sx={{ p: 2, textAlign: "center" }} className="fadeIn">
+        <Box p={2} textAlign="center" className="fadeIn">
           <Typography variant="subtitle1" fontWeight={700} mb={1}>
             {product.title}
           </Typography>
