efbeff6acf9c80145c5a2c6ca640206b48fca45e
elvisuriel <urielmarciales@gmil.com>
2023-09-18 09:29:19 -0500

    cambios en el carrito de compras

--- components/ui/ItemCounter.tsx
+++ components/ui/ItemCounter.tsx
@@ -1,17 +1,34 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Box, IconButton, Typography } from "@mui/material";
 import { AddCircleOutline, RemoveCircleOutline } from "@mui/icons-material";
 
-interface Props {}
+interface Props {
+  cantidad: number; // Prop para la cantidad actual
+  setCantidad: (cantidad: number) => void; // Función para actualizar la cantidad
+}
 
-export const ItemCounter: FC<Props> = () => {
+export const ItemCounter: FC<Props> = ({ cantidad, setCantidad }) => {
+  // Función para aumentar la cantidad
+  const incrementarCantidad = () => {
+    setCantidad(cantidad + 1);
+  };
+
+  // Función para disminuir la cantidad (no puede ser menor que 1)
+  const disminuirCantidad = () => {
+    if (cantidad > 1) {
+      setCantidad(cantidad - 1);
+    }
+  };
+
   return (
     <Box display="flex" alignItems="center">
-      <IconButton>
+      <IconButton onClick={disminuirCantidad}>
         <RemoveCircleOutline />
       </IconButton>
-      <Typography sx={{ width: 40, textAlign: "center" }}> 1 </Typography>
-      <IconButton>
+      <Typography sx={{ width: 40, textAlign: "center" }}>
+        {cantidad}
+      </Typography>
+      <IconButton onClick={incrementarCantidad}>
         <AddCircleOutline />
       </IconButton>
     </Box>
